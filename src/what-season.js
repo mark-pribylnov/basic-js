626f95b9fee69034a55fe6092d913a0552e5da41
mark-pribylnov <mark.pribylnov@gmail.com>
2023-04-15 19:30:08 +0300

    feat: solve seasons

--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,58 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (date === undefined) {
+    console.log("Unable to determine the time of year!");
+    return "Unable to determine the time of year!";
+  }
+
+  if (
+    typeof date === "string" ||
+    typeof date === "number" ||
+    typeof date === "function" ||
+    date.toString() === "[object Object]" ||
+    Array.isArray(date) ||
+    Object.getOwnPropertyNames(date).length
+  ) {
+    throw new Error("Invalid date!");
+  }
+
+  let month;
+
+  try {
+    month = date.getMonth();
+  } catch {}
+
+  if (month === 11 || month <= 1) {
+    console.log("winter");
+    return "winter";
+  } else if (month >= 2 && month < 5) {
+    console.log("spring");
+    return "spring";
+  } else if (month >= 5 && month < 8) {
+    console.log("summer");
+    return "summer";
+  } else if (month >= 8 && month < 11) {
+    console.log("autumn (fall)");
+    return "autumn (fall)";
+  }
+
+  throw new Error("Invalid date!");
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
--- test.js
+++ test.js
@@ -2,29 +2,77 @@
 
 // 1. get the sum of the first line
 
-const matrixSum = matrix => {
-  let sum = 0;
-
-  sum += matrix[0].reduce((sum, current) => sum + current, 0);
-
-  for (let i = 1; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i - 1][j] !== 0) {
-        sum += matrix[i][j];
-      }
-    }
+function getSeason(date) {
+  if (date === undefined) {
+    console.log("Unable to determine the time of year!");
+    return "Unable to determine the time of year!";
   }
-  console.log(sum);
-  return sum;
+
+  if (
+    typeof date === "string" ||
+    typeof date === "number" ||
+    typeof date === "function" ||
+    date.toString() === "[object Object]" ||
+    Array.isArray(date) ||
+    Object.getOwnPropertyNames(date).length === 9
+  ) {
+    throw new Error("Invalid date!");
+  }
+
+  let month;
+
+  try {
+    month = date.getMonth();
+  } catch {}
+
+  if (month === 11 || month <= 1) {
+    console.log("winter");
+    return "winter";
+  } else if (month >= 2 && month < 5) {
+    console.log("spring");
+    return "spring";
+  } else if (month >= 5 && month < 8) {
+    console.log("summer");
+    return "summer";
+  } else if (month >= 8 && month < 11) {
+    console.log("autumn (fall)");
+    return "autumn (fall)";
+  }
+  throw new Error("Invalid date!");
+}
+
+const deeperFakeDate = {
+  toString() {
+    return Date.prototype.toString.call(new Date());
+  },
+  getMonth() {
+    return Date.prototype.getMonth.call(new Date());
+  },
+  getFullYear() {
+    return Date.prototype.getFullYear.call(new Date(1994, 1, 2, 3, 4, 5));
+  },
+  getDate() {
+    return Date.prototype.getDate.call(new Date(2020, 0, 3, 4, 5, 6));
+  },
+  getHours() {
+    return Date.prototype.getHours.call(new Date(1978, 2, 4, 5, 6, 7));
+  },
+  getMinutes() {
+    return Date.prototype.getMinutes.call(new Date(202, 3, 5, 6, 7, 8));
+  },
+  getSeconds() {
+    return Date.prototype.getSeconds.call(new Date(1, 4, 6, 7, 8, 9));
+  },
+  getMilliseconds() {
+    return Date.prototype.getMilliseconds.call(new Date(2019, 7, 8, 9, 10, 11));
+  },
+  getDay() {
+    return Date.prototype.getDay.call(new Date(1812, 8, 9, 10, 11, 12));
+  },
+  [Symbol.toStringTag]: "Date",
 };
 
-matrixSum([
-  [0, 1],
-  [0, 5],
-]);
+// console.log(Object.getOwnPropertyNames(deeperFakeDate).length);
+// console.log(Object.getOwnPropertyNames());
 
-// matrixSum([
-//   [0, 1, 1, 2],
-//   [0, 5, 0, 0],
-//   [2, 0, 3, 3],
-// ]);
+getSeason(deeperFakeDate);
