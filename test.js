6cdf4639184b2be2a6fda2fbfb9b3c433428c252
mark-pribylnov <mark.pribylnov@gmail.com>
2023-04-15 16:12:38 +0300

    feat: solve count cats task

--- test.js
+++ test.js
@@ -1,80 +1,21 @@
 "use strict";
 
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-  }
-}
-
-class BinarySearchTree {
-  constructor() {
-    this.rootNode = null;
-  }
-
-  root() {
-    return this.rootNode;
-  }
-
-  add(data) {
-    this.rootNode = insert(this.rootNode, data);
-
-    function insert(node, value) {
-      if (node === null) {
-        return new Node(value);
-      }
-
-      if (node.data === value) {
-        return node;
-      }
-
-      if (node.data > value) {
-        node.left = insert(node.left, data);
-      } else {
-        node.right = insert(node.right, data);
-      }
-
-      return node;
-    }
-  }
-
-  has(data) {
-    function findNode(node, data) {
-      if (node === null) {
-        return false;
-      }
-
-      if (node.data === data) {
-        return true;
-      }
-
-      if (data < node.data) {
-        return findNode(node.left, data);
-      } else {
-        return findNode(node.right, data);
+const countCats = arr => {
+  let catCounter = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      // console.log(arr[i][j]);
+      if (arr[i][j] === "^^") {
+        catCounter++;
       }
     }
-
-    return findNode(this.rootNode, data);
   }
-}
+  console.log(catCounter);
+  return catCounter;
+};
 
-const tree1 = new BinarySearchTree();
-
-tree1.add(30);
-tree1.add(45);
-tree1.add(57);
-tree1.add(35);
-tree1.add(13);
-tree1.add(36);
-tree1.add(17);
-tree1.add(78);
-tree1.add(7);
-tree1.add(14);
-
-console.log(tree1.has(30));
-
-console.log("\n\nFINAL TREE:\n", tree1);
-
-// tree1.has(10);
+countCats([
+  [0, 1, "^^"],
+  [0, "^^", 2],
+  ["^^", 1, 2],
+]);
