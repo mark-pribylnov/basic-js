69ebe96ccd493e7f600757e88846fb28c6b55d32
mark-pribylnov <mark.pribylnov@gmail.com>
2023-04-15 21:16:32 +0300

    feat: solve dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,45 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(members)) return false;
+
+  if (members.length === 0) {
+    console.log(false);
+    return false;
+  }
+
+  let teamName = [];
+  for (let person of members) {
+    if (typeof person === "string") {
+      for (let char of person) {
+        if (char !== " ") {
+          teamName.push(char.toUpperCase());
+          break;
+        }
+      }
+    }
+  }
+
+  teamName = teamName.sort().join("");
+  console.log(teamName);
+  return teamName;
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
--- test.js
+++ test.js
@@ -1,78 +1,48 @@
 "use strict";
 
-// 1. get the sum of the first line
+function dreamTeam(members) {
+  if (!Array.isArray(members)) return false;
 
-function getSeason(date) {
-  if (date === undefined) {
-    console.log("Unable to determine the time of year!");
-    return "Unable to determine the time of year!";
+  if (members.length === 0) {
+    console.log(false);
+    return false;
   }
 
-  if (
-    typeof date === "string" ||
-    typeof date === "number" ||
-    typeof date === "function" ||
-    date.toString() === "[object Object]" ||
-    Array.isArray(date) ||
-    Object.getOwnPropertyNames(date).length === 9
-  ) {
-    throw new Error("Invalid date!");
+  let teamName = [];
+  for (let person of members) {
+    if (typeof person === "string") {
+      for (let char of person) {
+        if (char !== " ") {
+          teamName.push(char.toUpperCase());
+          break;
+        }
+      }
+    }
   }
 
-  let month;
-
-  try {
-    month = date.getMonth();
-  } catch {}
-
-  if (month === 11 || month <= 1) {
-    console.log("winter");
-    return "winter";
-  } else if (month >= 2 && month < 5) {
-    console.log("spring");
-    return "spring";
-  } else if (month >= 5 && month < 8) {
-    console.log("summer");
-    return "summer";
-  } else if (month >= 8 && month < 11) {
-    console.log("autumn (fall)");
-    return "autumn (fall)";
-  }
-  throw new Error("Invalid date!");
+  teamName = teamName.sort().join("");
+  console.log(teamName);
+  return teamName;
 }
 
-const deeperFakeDate = {
-  toString() {
-    return Date.prototype.toString.call(new Date());
-  },
-  getMonth() {
-    return Date.prototype.getMonth.call(new Date());
-  },
-  getFullYear() {
-    return Date.prototype.getFullYear.call(new Date(1994, 1, 2, 3, 4, 5));
-  },
-  getDate() {
-    return Date.prototype.getDate.call(new Date(2020, 0, 3, 4, 5, 6));
-  },
-  getHours() {
-    return Date.prototype.getHours.call(new Date(1978, 2, 4, 5, 6, 7));
-  },
-  getMinutes() {
-    return Date.prototype.getMinutes.call(new Date(202, 3, 5, 6, 7, 8));
-  },
-  getSeconds() {
-    return Date.prototype.getSeconds.call(new Date(1, 4, 6, 7, 8, 9));
-  },
-  getMilliseconds() {
-    return Date.prototype.getMilliseconds.call(new Date(2019, 7, 8, 9, 10, 11));
-  },
-  getDay() {
-    return Date.prototype.getDay.call(new Date(1812, 8, 9, 10, 11, 12));
-  },
-  [Symbol.toStringTag]: "Date",
-};
+dreamTeam([
+  "   William Alston ",
+  " Paul Benacerraf",
+  "  Ross Cameron",
+  "       Gilles Deleuze",
+  "  Arda Denkel ",
+  "  Michael Devitt",
+  "  Kit Fine",
+  " Nelson Goodman",
+  "David Kolb",
+  "   Saul Kripke",
+  "  Trenton Merricks",
+  "  Jay Rosenberg",
+]);
+//ADGJKMNPRSTW
 
-// console.log(Object.getOwnPropertyNames(deeperFakeDate).length);
-// console.log(Object.getOwnPropertyNames());
-
-getSeason(deeperFakeDate);
+// for (let char of "   William Alston ") {
+//   if (char !== " ") {
+//     console.log(char);
+//   }
+// }
