c2609dc05815612cbe863b6bbd4e1953a9f78ec3
mark-pribylnov <mark.pribylnov@gmail.com>
2023-04-16 15:15:19 +0300

    feat: solve transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,67 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!(arr instanceof Array)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  if (
+    !arr.includes("--discard-prev") &&
+    !arr.includes("--discard-next") &&
+    !arr.includes("--double-prev") &&
+    !arr.includes("--double-next")
+  ) {
+    return arr;
+  }
+
+  const input = [];
+
+  for (let el of arr) {
+    input.push(el);
+  }
+
+  const output = [];
+
+  for (let i = 0; i < input.length; i++) {
+    if (typeof input[i] === "number") {
+      output.push(input[i]);
+    }
+
+    if (input[i] === "--discard-prev") {
+      if (typeof input[i - 1] === "number") {
+        output.pop();
+        // output.push(input[i - 1]);
+      }
+    } else if (input[i] === "--double-prev") {
+      if (typeof input[i - 1] === "number") {
+        output.push(input[i - 1]);
+      }
+    } else if (input[i] === "--discard-next") {
+      input.splice([i + 1], 1);
+    } else if (input[i] === "--double-next") {
+      input.splice(i + 1, 0, input[i + 1]);
+    }
+  }
+
+  // console.log("Output", output);
+  return output;
 }
 
 module.exports = {
-  transform
+  transform,
 };
--- test.js
+++ test.js
@@ -1,48 +1,72 @@
 "use strict";
 
-function dreamTeam(members) {
-  if (!Array.isArray(members)) return false;
+// Discard prev
+// 1. Add elements to a new array until you face the condition
+// 2. delete element at the position before the condition
 
-  if (members.length === 0) {
-    console.log(false);
-    return false;
+function transform(arr) {
+  if (!(arr instanceof Array)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
   }
 
-  let teamName = [];
-  for (let person of members) {
-    if (typeof person === "string") {
-      for (let char of person) {
-        if (char !== " ") {
-          teamName.push(char.toUpperCase());
-          break;
-        }
+  if (
+    !arr.includes("--discard-prev") &&
+    !arr.includes("--discard-next") &&
+    !arr.includes("--double-prev") &&
+    !arr.includes("--double-next")
+  ) {
+    return arr;
+  }
+
+  const input = [];
+
+  for (let el of arr) {
+    input.push(el);
+  }
+
+  const output = [];
+
+  for (let i = 0; i < input.length; i++) {
+    if (typeof input[i] === "number") {
+      output.push(input[i]);
+    }
+
+    if (input[i] === "--discard-prev") {
+      if (typeof input[i - 1] === "number") {
+        output.pop();
+        // output.push(input[i - 1]);
       }
+    } else if (input[i] === "--double-prev") {
+      if (typeof input[i - 1] === "number") {
+        output.push(input[i - 1]);
+      }
+    } else if (input[i] === "--discard-next") {
+      input.splice([i + 1], 1);
+    } else if (input[i] === "--double-next") {
+      input.splice(i + 1, 0, input[i + 1]);
     }
   }
 
-  teamName = teamName.sort().join("");
-  console.log(teamName);
-  return teamName;
+  // console.log("Output", output);
+  return output;
 }
 
-dreamTeam([
-  "   William Alston ",
-  " Paul Benacerraf",
-  "  Ross Cameron",
-  "       Gilles Deleuze",
-  "  Arda Denkel ",
-  "  Michael Devitt",
-  "  Kit Fine",
-  " Nelson Goodman",
-  "David Kolb",
-  "   Saul Kripke",
-  "  Trenton Merricks",
-  "  Jay Rosenberg",
-]);
-//ADGJKMNPRSTW
+// console.log(![1, 2, 3].includes(2));
 
-// for (let char of "   William Alston ") {
-//   if (char !== " ") {
-//     console.log(char);
-//   }
-// }
+// transform([1, 2, 3, 4, 5, 6]);
+// transform([1, 2, 3, "--discard-next", 1337, "--double-prev", 4, 5]);
+// output: [1, 2, 3, 4, 5]
+// transform([1, 2, 3, "--double-next", 1337, "--double-prev", 4, 5]);
+// output: [1, 2, 3, 1337, 1337, 1337, 4, 5]
+// transform([1, 2, 3, "--discard-next", 1337, "--discard-prev", 4, 5]);
+// output: [1, 2, 3, 4, 5]
+// transform([1, 2, 3, "--double-next", 1337, "--discard-prev", 4, 5]);
+// output: [1, 2, 3, 1337, 4, 5]
+
+// transform([1, 2, 3, "--discard-next", 1337, "--double-prev", 4, 5]); // => [1, 2, 4, 5]
+// transform([1, 2, 3, 1337, "--double-prev", 4, 5]); // => [1, 2, 4, 5]
+// transform([1, 2, 3, "--discard-next", 4, 5]); // => [1, 2, 4, 5]
+// transform([1, 2, 3, "--double-next", 4, 5]); //=> [1, 2, 3, 4, 4, 5]
+// transform([1, 2, 3, "--double-prev", 4, 5]); // => [1, 2, 4, 5]
+
+// console.log("        jjjhjhjh");
