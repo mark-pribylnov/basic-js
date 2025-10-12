491b8899cb54f6edbe85774f72e97766defe6027
unknown <mark.pribylnov@gmail.com>
2025-10-12 20:33:56 +0300

    feat: solve a task

--- src/encode-line.js
+++ src/encode-line.js
@@ -11,8 +11,40 @@ const { NotImplementedError } = require("../lib");
  *
  */
 
-function encodeLine(str) {}
-console.log(encodeLine("aabbbc"));
+function encodeLine(str) {
+  function getCharClones(string, index, counter) {
+    const nextChar = string[index + 1];
+    const currentChar = string[index];
+    if (nextChar !== currentChar) return [counter, index];
+
+    return getCharClones(str, index + 1, (counter += 1));
+  }
+
+  let finalStr = "";
+
+  for (let i = 0; i <= str.length - 1; ) {
+    const nextChar = str[i + 1];
+    const currentChar = str[i];
+
+    if (nextChar === currentChar) {
+      const [counter, lastCheckedIndex] = getCharClones(str, i, 1);
+      finalStr += `${counter}${currentChar}`;
+      i = lastCheckedIndex + 1;
+    } else {
+      finalStr += currentChar;
+      i += 1;
+    }
+  }
+
+  return finalStr;
+}
+// just inside the loop in the body if the next char is the same do i+=2 and write "n"char else write the char as it is.
+// console.log(encodeLine("aabbbc")); //3b2ac
+// console.log(encodeLine("aaaatttt"));
+// console.log(encodeLine("aabbccc"));
+// console.log(encodeLine("abbcca"));
+// console.log(encodeLine("xyz"));
+// console.log(encodeLine(""));
 
 module.exports = {
   encodeLine,
--- test/encode-line.test.js
+++ test/encode-line.test.js
@@ -1,20 +1,20 @@
-const { describe } = require('node:test');
-const assert = require('node:assert');
-const { test } = require('../lib');
-const { encodeLine } = require('../src/encode-line.js');
+const { describe } = require("node:test");
+const assert = require("node:assert");
+const { test } = require("../lib");
+const { encodeLine } = require("../src/encode-line.js");
 
-describe('Encode line', () => {
+describe("Encode line", () => {
   // Presence requirement
-  test('function encodeLine exists', () => {
-    assert.strictEqual(typeof encodeLine, 'function');
+  test("function encodeLine exists", () => {
+    assert.strictEqual(typeof encodeLine, "function");
   });
 
   // Functional requirements
-  test('should return encoding version of string', () => {
-    assert.strictEqual(encodeLine('aaaatttt'), '4a4t');
-    assert.strictEqual(encodeLine('aabbccc'), '2a2b3c');
-    assert.strictEqual(encodeLine('abbcca'), 'a2b2ca');
-    assert.strictEqual(encodeLine('xyz'), 'xyz');
-    assert.strictEqual(encodeLine(''), '');
+  test("should return encoding version of string", () => {
+    assert.strictEqual(encodeLine("aaaatttt"), "4a4t");
+    assert.strictEqual(encodeLine("aabbccc"), "2a2b3c");
+    assert.strictEqual(encodeLine("abbcca"), "a2b2ca");
+    assert.strictEqual(encodeLine("xyz"), "xyz");
+    assert.strictEqual(encodeLine(""), "");
   });
 });
