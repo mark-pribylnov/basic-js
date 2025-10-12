38db3f69c13a01c06c18cfd83450f598f974eec6
unknown <mark.pribylnov@gmail.com>
2025-10-12 14:11:24 +0300

    feat: solve a task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -12,11 +12,63 @@ const { NotImplementedError } = require('../lib');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 
-function getCommonCharacterCount(/* s1, s2 */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  function getCommonChars(str1, str2) {
+    const arr = [];
+
+    str1.split("").forEach(char => {
+      if (!arr.includes(char) && str2.includes(char)) {
+        arr.push(char);
+      }
+    });
+
+    return arr;
+  }
+  function countCommonIndiviually(commonChars, str) {
+    return commonChars.map(char => {
+      const counter = str.split("").reduce((acc, strChar) => {
+        if (char === strChar) return (acc += 1);
+        return acc;
+      }, 0);
+
+      return [char, counter];
+    });
+  }
+  function calcCommonChars(arr1, arr2) {
+    let commonCounter = 0;
+
+    arr1.forEach((el, ind) => {
+      let biggerNum = null;
+      let lowerNum = null;
+
+      if (el[1] > arr2[ind][1]) {
+        biggerNum = el[1];
+        lowerNum = arr2[ind][1];
+      } else {
+        biggerNum = arr2[ind][1];
+        lowerNum = el[1];
+      }
+
+      commonCounter += lowerNum;
+    });
+
+    return commonCounter;
+  }
+
+  const commonChars = getCommonChars(s1, s2);
+
+  const firstCommonChars = countCommonIndiviually(commonChars, s1);
+  const secondCommonChars = countCommonIndiviually(commonChars, s2);
+
+  return calcCommonChars(firstCommonChars, secondCommonChars);
 }
 
+console.log(getCommonCharacterCount("aabcc", "adcaa")); //3
+console.log(getCommonCharacterCount("abca", "xyzbac")); //3
+console.log(getCommonCharacterCount("zzzz", "zzzzzzzz")); //4
+console.log(getCommonCharacterCount("", "abc")); //0
+console.log(getCommonCharacterCount("a", "b")); //0
+
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
--- test/common-character-count.test.js
+++ test/common-character-count.test.js
@@ -1,22 +1,22 @@
-const { describe } = require('node:test');
-const assert = require('node:assert');
-const { test } = require('../lib');
-const { getCommonCharacterCount } = require('../src/common-character-count.js');
+const { describe } = require("node:test");
+const assert = require("node:assert");
+const { test } = require("../lib");
+const { getCommonCharacterCount } = require("../src/common-character-count.js");
 
-describe('Common character count', () => {
+describe("Common character count", () => {
   // Presence requirement
-  describe('function presence', () => {
-    test('function getCommonCharacterCount exists', () => {
-      assert.strictEqual(typeof getCommonCharacterCount, 'function');
+  describe("function presence", () => {
+    test("function getCommonCharacterCount exists", () => {
+      assert.strictEqual(typeof getCommonCharacterCount, "function");
     });
   });
 
   // Functional requirements
-  test('should return the number of common characters between the given strings', () => {
-    assert.strictEqual(getCommonCharacterCount('aabcc', 'adcaa'), 3);
-    assert.strictEqual(getCommonCharacterCount('zzzz', 'zzzzzzz'), 4);
-    assert.strictEqual(getCommonCharacterCount('abca', 'xyzbac'), 3);
-    assert.strictEqual(getCommonCharacterCount('', 'abc'), 0);
-    assert.strictEqual(getCommonCharacterCount('a', 'b'), 0);
+  test("should return the number of common characters between the given strings", () => {
+    assert.strictEqual(getCommonCharacterCount("aabcc", "adcaa"), 3);
+    assert.strictEqual(getCommonCharacterCount("zzzz", "zzzzzzz"), 4);
+    assert.strictEqual(getCommonCharacterCount("abca", "xyzbac"), 3);
+    assert.strictEqual(getCommonCharacterCount("", "abc"), 0);
+    assert.strictEqual(getCommonCharacterCount("a", "b"), 0);
   });
 });
