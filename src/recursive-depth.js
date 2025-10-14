bdf765e5f1d98705f0fc69c780b4a90b4199e598
unknown <mark.pribylnov@gmail.com>
2025-10-14 21:41:42 +0300

    feat: solve a task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Implement class DepthCalculator with method calculateDepth
@@ -13,12 +13,108 @@ const { NotImplementedError } = require('../lib');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  calculateDepth(arr, depth = 0) {
+    if (!arr.some(el => Array.isArray(el))) {
+      return 1;
+    }
+
+    const depths = [];
+    // let lastChecked = null;
+
+    function goDeeper(arr, depth = 0) {
+      // const currentArray = arr;
+      // Base case
+      if (arr.length === 0 || !arr.some(el => Array.isArray(el))) return depth + 1;
+      // Continues case: recursion if array contains only an array
+      if (arr.length === 1 && Array.isArray(arr[0])) return goDeeper(arr[0], depth + 1);
+
+      // go find an array
+      for (const el of arr) {
+        // lastChecked = el;
+        if (Array.isArray(el)) {
+          const currentDepth = goDeeper(el, depth + 1);
+          depths.push(currentDepth);
+        }
+      }
+
+      return depth + 1;
+    }
+
+    goDeeper(arr);
+    return depths.sort((a, b) => b - a)[0];
   }
 }
+// const obj = new DepthCalculator();
+
+// function calculateDepth(arr, depth = 0) {
+//   const depths = [];
+//   let lastChecked = null;
+
+//   function goDeeper(arr, depth = 0) {
+//     const currentArray = arr;
+//     // Base case
+//     if (arr.length === 0 || !arr.some(el => Array.isArray(el))) return depth + 1;
+//     // Continues case: recursion if array contains only an array
+//     if (arr.length === 1 && Array.isArray(arr[0])) return goDeeper(arr[0], depth + 1);
+
+//     // go find an array
+//     for (const el of arr) {
+//       lastChecked = el;
+//       if (Array.isArray(el)) {
+//         const currentDepth = goDeeper(el, depth + 1);
+//         depths.push(currentDepth);
+//       }
+//     }
+
+//     return depth + 1;
+//   }
+
+//   goDeeper(arr);
+//   return depths.sort((a, b) => b - a)[0];
+// }
+// console.log(calculateDepth([[[]]]));
+// console.log(calculateDepth([1, [[[[[[101, 123, [5543]]]]], 10], 4, [1], 5], 6, 7, [9, 10]]));
+// console.log(
+//   calculateDepth([
+//     1,
+//     [8, [[]]],
+//     [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]],
+//     2,
+//     3,
+//     [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]],
+//     [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]],
+//     4,
+//     5,
+//     ["6575", ["adas", ["dfg", [0]]]],
+//   ])
+// );
+// console.log(depths);
+// console.log(obj.calculateDepth([1, 2, 3, [4, 5]]));
+
+// depthCalculator.calculateDepth([1, 2]);
+// function calculateDepth(arr, depth) {
+//
+// }
 
 module.exports = {
   depthCalculator: new DepthCalculator(),
 };
+
+// function calculateDepth(arr, depth = 0) {
+//   const currentArray = arr;
+//   // Base case
+//   if (arr.length === 0 || !arr.some(el => Array.isArray(el))) return depth + 1;
+//   // Continues case: recursion if array contains only an array
+//   if (arr.length === 1 && Array.isArray(arr[0])) return calculateDepth(arr[0], depth + 1);
+
+//   // go find an array
+//   for (const el of arr) {
+//     lastChecked = el;
+//     if (Array.isArray(el)) {
+//       const currentDepth = calculateDepth(el, depth + 1);
+//       depths.push(currentDepth);
+//     }
+//   }
+
+//   return depth + 1;
+// }
--- test/recursive-depth.test.js
+++ test/recursive-depth.test.js
@@ -1,30 +1,30 @@
-const { describe } = require('node:test');
-const assert = require('node:assert');
-const { test } = require('../lib');
-const { depthCalculator } = require('../src/recursive-depth.js');
+const { describe } = require("node:test");
+const assert = require("node:assert");
+const { test } = require("../lib");
+const { depthCalculator } = require("../src/recursive-depth.js");
 
 const calculateDepth = depthCalculator.calculateDepth.bind(depthCalculator);
 
-const createFlatArr = (length) => Array.from({ length }, () => Math.floor(Math.random() * length));
+const createFlatArr = length => Array.from({ length }, () => Math.floor(Math.random() * length));
 
-describe('Recursive depth', () => {
+describe("Recursive depth", () => {
   // Presence requirement
-  describe('function presence', () => {
-    test('function calculateDepth exists', () => {
-      assert.strictEqual(typeof calculateDepth, 'function');
+  describe("function presence", () => {
+    test("function calculateDepth exists", () => {
+      assert.strictEqual(typeof calculateDepth, "function");
     });
   });
 
   // Functional requirements
-  describe('functional requirements', () => {
-    test('returns correct depth of flat arrays', () => {
+  describe("functional requirements", () => {
+    test("returns correct depth of flat arrays", () => {
       for (let i = 1; i < 100; i++) {
         const flatArr = createFlatArr(i);
         assert.strictEqual(calculateDepth(flatArr), 1);
       }
     });
-    
-    test('returns correct depth of nested arrays', () => {
+
+    test("returns correct depth of nested arrays", () => {
       assert.strictEqual(
         calculateDepth([
           1,
@@ -36,12 +36,12 @@ describe('Recursive depth', () => {
           [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]],
           4,
           5,
-          ['6575', ['adas', ['dfg', [0]]]],
+          ["6575", ["adas", ["dfg", [0]]]],
         ]),
-        31,
+        31
       );
       assert.strictEqual(calculateDepth([1, 2, 3, 4, 5, [1]]), 2);
-      assert.strictEqual(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]), 5);
+      assert.strictEqual(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ["6575", ["adas", ["dfg", [0]]]]]), 5);
       assert.strictEqual(calculateDepth([1, 2, 3, [8, [2]], 4, 5, []]), 3);
       assert.strictEqual(calculateDepth([1, 2, 3, [1], 4, 5, [1]]), 2);
       assert.strictEqual(
@@ -54,14 +54,14 @@ describe('Recursive depth', () => {
           [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]],
           4,
           5,
-          ['6575', ['adas', ['dfg', [0]]]],
+          ["6575", ["adas", ["dfg", [0]]]],
         ]),
-        25,
+        25
       );
       assert.strictEqual(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, []]), 4);
       assert.strictEqual(
-        calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ['6575', ['adas', ['dfg', [0]]]]]),
-        15,
+        calculateDepth([1, [8, [[]]], 2, 3, [8, [[[[[[[[[[[[[]]]]]]]]]]]]]], 4, 5, ["6575", ["adas", ["dfg", [0]]]]]),
+        15
       );
     });
 
