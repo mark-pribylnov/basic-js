9d8fadcdb501b305d64add5365a87426a8817f1c
unknown <mark.pribylnov@gmail.com>
2025-10-15 21:11:20 +0300

    feat: solve a task

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,34 @@ const { NotImplementedError } = require('../lib');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function sortByHeight(arr) {
+  const positions = [];
+  const sort = [];
+  const final = [];
+
+  arr.forEach((el, i) => {
+    if (el === -1) {
+      positions.push(i);
+    } else {
+      sort.push(el);
+    }
+  });
+
+  sort.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (positions.includes(i)) {
+      final.push(-1);
+    } else {
+      const taken = sort.shift();
+      final.push(taken);
+    }
+  }
+
+  return final;
 }
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
--- test/sort-by-height.test.js
+++ test/sort-by-height.test.js
@@ -1,23 +1,23 @@
-const { describe } = require('node:test');
-const assert = require('node:assert');
-const { test } = require('../lib');
-const { sortByHeight } = require('../src/sort-by-height.js');
+const { describe } = require("node:test");
+const assert = require("node:assert");
+const { test } = require("../lib");
+const { sortByHeight } = require("../src/sort-by-height.js");
 
-describe('Sort by height', () => {
+describe("Sort by height", () => {
   // Presence requirement
-  test('function sortByHeight exists', () => {
-    assert.strictEqual(typeof sortByHeight, 'function');
+  test("function sortByHeight exists", () => {
+    assert.strictEqual(typeof sortByHeight, "function");
   });
 
   // Functional requirements
-  test('should return sorted array without moving -1', () => {
+  test("should return sorted array without moving -1", () => {
     assert.deepStrictEqual(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]), [-1, 150, 160, 170, -1, -1, 180, 190]);
     assert.deepStrictEqual(sortByHeight([-1, -1, -1, -1, -1]), [-1, -1, -1, -1, -1]);
     assert.deepStrictEqual(sortByHeight([-1]), [-1]);
     assert.deepStrictEqual(sortByHeight([4, 2, 9, 11, 2, 16]), [2, 2, 4, 9, 11, 16]);
     assert.deepStrictEqual(
       sortByHeight([23, 54, -1, 43, 1, -1, -1, 77, -1, -1, -1, 3]),
-      [1, 3, -1, 23, 43, -1, -1, 54, -1, -1, -1, 77],
+      [1, 3, -1, 23, 43, -1, -1, 54, -1, -1, -1, 77]
     );
   });
 });
